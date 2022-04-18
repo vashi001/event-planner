67941f8169d9447127e025b43f7d789232156a98
vashi001 <101816262+vashi001@users.noreply.github.com>
2022-04-18 22:46:46 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyADUyPQjaDy8tfSqCOHhsHJi4EOuWcQubM",
+  authDomain: "event-planner-15fe6.firebaseapp.com",
+  projectId: "event-planner-15fe6",
+  storageBucket: "event-planner-15fe6.appspot.com",
+  messagingSenderId: "956947028595",
+  appId: "1:956947028595:web:6c5e7e9668c5e3e1a36806",
+  measurementId: "G-JEVLY6Q87T"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
